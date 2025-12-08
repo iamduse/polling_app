0a044eba7b9a948b29006d79a0c1f232f8b41710
90duse <duseibnuduse1212@gmail.com>
2025-12-08 12:42:25 +0300

    completed login and logout logic

--- poll_app/urls.py
+++ poll_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("home" , views.home,  name="home"),
-    path("login/" , views.user_login,  name="login"),
-    #path("/logout/" , views.user_logout,  name="logout")
+    #path("login/" , views.user_login,  name="login"),
+    path("logout" , views.user_logout,  name="logout")
 
 ]
--- poll_app/views.py
+++ poll_app/views.py
@@ -6,29 +6,33 @@ from django.contrib import messages
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html', {})
+     #return render(request, 'index.html', {})
+  # check if the user is login in
+     if request.method == "POST":
+         email = request.POST["email"]
+         password = request.POST["password"]
+         user = authenticate(request, username=email, password=password)
+         if user is not None:
+             login(request, user)
+             messages.success(request, "Successfully Login In. Now You can VoteVote 😎 ")
+             return redirect("home")
+         else:
+             messages.error(request, "There is error please login in again")
+             return redirect("login")
+     else:
+         return render(request, 'login.html', {})
+
 
 
 
 def user_login(request):
-    # check if the user is login in
-    if request.method == "POST":
-        email = request.POST["email"]
-        password = request.POST["password"]
-        user = authenticate(request, username=email, password=password)
-        if user is not None:
-            login(request, user)
-            messages.success(request, "Successfully Login In. Now You can VoteVote 😎 ")
-            return redirect("home")
-        else:
-            messages.error(request, "There is error please login in again")
-            return redirect("home")
-    else:
-        return render(request, 'login.html', {})
+    pass
+
+
 
 
 
 def user_logout(request):
     logout(request)
     messages.success(request, message="You have been logout successfully. see you again ")
-    return  redirect("user_login")
+    return  redirect("home")
